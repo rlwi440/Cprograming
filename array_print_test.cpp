0604b987a7bbb83ed3c2f4ae872a0fc1f30d7861
bae_giweon <rlwi440@naver.com>
2020-04-19 22:01:43 +0900

    [update] added main function 347 page

--- array_print_test.cpp
+++ array_print_test.cpp
@@ -54,7 +54,7 @@ int main(void)
      printf("바꾸기 전 문자열: %s\n",str);
      my_strcpy(str,"apple"); // 문자열 "apple" 복사
      printf("바꾼 후 문자열 후:%s\n",str);
-     printf("다른 문자열 대입: %s\n",my_strcpy(str,"kiwi")); //반홥값으로 출력
+     printf("다른 문자열 대입: %s\n",my_strcpy(str,"kiwi")); //반환값으로 출력
 
      return 0;
  }
