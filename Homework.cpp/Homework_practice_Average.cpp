958c5e47816565280d47896b4ed7f0686106c94d
bae_giweon <rlwi440@naver.com>
2020-06-18 21:46:46 +0900

    [add] example average and standard diviation program

--- Homework.cpp/Homework_practice_Average.cpp
+++ Homework.cpp/Homework_practice_Average.cpp
@@ -27,8 +27,6 @@ void main()
     }
     avg = sum / n;
     printf("The average is %5.2f\n", avg);
-
-    for (count = 0; count < n; ++count)
     {
         d = list[count] - avg;
         printf("i=%d x= %5.2f d =%5.2f\n", count + 1, list[count], d);
